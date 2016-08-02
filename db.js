ac5e7818a13631225158d8e99cf28ca08a77e998
Robert van der Mark <robertvandermark@Roberts-MBP.home>
2016-08-02 12:45:17 -0400

    all functional.  still not pretty

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var _data = {
 		{name: "Ejector Seat"},
 		{name: "Razor-wire Watch"}
 		],
-	UnderWare: [
+	UnderWear: [
 		{name: 'Hanes'},
 		{name: 'Fruit of the Loom'}
 		]
@@ -14,9 +14,43 @@ function listCat()	{
 	return Object.keys(_data);
 };
 
-function addCat(newCategory)	{
-	// _data.newCategory = "ouch";
-	console.log(typeof _data)  
-}
+function listProd(category) {
+	var list =[];
+	_data[category].forEach(function(productObj){
+		 list.push(productObj.name);
+	})
+	return list;
+};
 
-module.exports = {listCat: listCat, addCat: addCat,}
+function addCat(newCategory)	{
+	_data[newCategory] = []; 
+};
+
+function addProd(category, product){
+	_data[category].push({"name": product});
+};
+
+function delCat(category){
+	console.log('keel the ', category)
+	delete _data[category]
+	console.log(_data)
+};
+function delProd(category, product){
+	var index = 0;
+	var removeIndex = 0;
+	_data[category].forEach(function(prodObj){
+		if (prodObj.name === product){
+			removeIndex = index
+		}
+		index++;
+	});
+	console.log(removeIndex)
+	_data[category].splice(removeIndex,1)
+	console.log(_data[category])
+};
+
+
+
+module.exports = {listCat: listCat, addCat: addCat, listProd: listProd, addProd: addProd, delCat: delCat, delProd: delProd}
+
+
--- routes/categories.router.js
+++ routes/categories.router.js
@@ -2,27 +2,61 @@ var express = require('express');
 var router = express.Router();
 var database = require('../db.js' );
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 router.use(bodyParser.urlencoded({extended: false}));
-var categoriesList = database.listCat();
+router.use(bodyParser());
+router.use(methodOverride('_method'));
+
+function catList(){
+	return database.listCat();
+};
+function prodList(category){
+	return database.listProd(category);
+}
 
 router.get('/home', function(req, res){
-	
+	var categoriesList = catList();
 	res.render('index',{title: "Welcome to Acme Categories", showCatForm: true, categories: categoriesList})
 });
 
 router.get('/categories/:category', function(req,res){
-	console.log(req.params.category )
 	var cat = req.params.category;
-	res.render('index',{title: cat + ' Category', categories: categoriesList })
-
+	var categoriesList = catList();
+	var productsList = prodList(cat);
+	res.render('index',{title: cat + ' Category', categories: categoriesList , products: productsList, showProdForm: true,  category: cat});
 })
 
-router.post('/CreateCat', function(req,res){
-	console.log(req.body.category)
-	res.redirect('/');
+router.post('/home/CreateCat', function(req,res){
+	var newCategory = req.body.category;
+	database.addCat(newCategory);	 
+	res.redirect('/categories/' + newCategory);
 });
 
+
+router.post('/categories/:category/:product', function(req,res){
+	var newProduct = req.body.product;
+	var category = req.params.category;
+	if (category === 'DELETE'){
+		console.log('Delete!')
+	}
+	database.addProd(category,newProduct);
+	res.redirect('/categories/'+ category);
+})
+
+router.delete('/categories/:category/resource?', function(req, res){
+	var category = req.params.category;
+	database.delCat(category);
+	res.redirect('/home/')
+})
+
+router.delete('/categories/:category/:product/resource?', function(req,res){
+	var product = req.params.product;
+	var category = req.params.category;
+	database.delProd(category,product);
+	res.redirect('/categories/'+ category);
+
+})
 
 
 module.exports = router;
