aeac9ff98d4196edf08c17f7aeb47dcf0c467263
Robert van der Mark <robertvandermark@Roberts-MBP.home>
2016-08-08 13:10:52 -0400

    Created Category route

--- routes/categories.js
+++ routes/categories.js
@@ -6,18 +6,24 @@ module.exports = router;
 
 router.get('/:id', function(req,res){
 	var categoryId = req.params.id;
-	console.log('categoryId =', categoryId)
 
-	client.query('select categories.name as category_name, products.name from categories join products on categories.id=products.cat_id where categories.id = $1',[categoryId],  function(err, results){
-		 	if (err) throw err;
-			var products = results.rows;
-			var category = results.rows[0].category_name;
-			client.query('select name, id from categories', function(err, results){
+	// get the products list
+	client.query('select products.name from categories join products on categories.id=products.cat_id where categories.id = $1',[categoryId],  function(err, results){
+	 	if (err) throw err;
+	 	var products = '';
+		if (results.rows[0] !== undefined) {
+			products = results.rows;
+		}
+		
+		// Find category name
+		client.query('select categories.name from categories where categories.id = $1',[categoryId],  function(err, results){
+	 		if (err) throw err;
+	 		var category = results.rows[0].name;
+
+	 		// find all categories
+	 		client.query('select name, id from categories', function(err, results){
 				if(err) throw err;
 				var categories = results.rows;
-				console.log(products);
-				console.log(category);
-				console.log(categories)
 				res.render('category',{
 				    title: category + ' Category',
 				    categories: categories ,
@@ -25,15 +31,24 @@ router.get('/:id', function(req,res){
 				    category: category
 		  		});
 			})
+		})
 	})
-			
-
 });
 
 router.post('/', function(req, res){
 	var category = req.body.category;
-	client.addCategory(category);	 
-	res.redirect('/categories/' + category);
+	console.log(category)
+	client.query('insert into categories (id, name) values (DEFAULT, $1)',[category], function(err, results){
+		if(err) throw err;
+		client.query('select id from categories where name = $1', [category], function(err, results){
+			if (err) throw err;
+			res.redirect('/categories/' + results.rows[0].id);
+
+		})
+		
+	})
+
+	
 });
 
 
